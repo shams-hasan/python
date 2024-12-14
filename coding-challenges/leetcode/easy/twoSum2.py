43516040d57332cbd1c57e950cf8a0f212226303
Shams Hasan <105460072+shamshasan0@users.noreply.github.com>
2024-12-13 22:25:16 -0500

    Update twoSum2.py

--- coding-challenges/leetcode/easy/twoSum2.py
+++ coding-challenges/leetcode/easy/twoSum2.py
@@ -1,18 +1,5 @@
-from operator import index
-# LOOP THROUGH AN ARRAY AND FIND THE INDICES OF NUMBERS THAT ADD UP TO TARGET
+   # O(N) SOLUTION
 
-# PARAMS: ARR: ARRAY, TARGET: NUMBER
-# RETURN: INDICES
-class Solution():
-    # O(N^2)
-    def twoSum1(arr, target):
-        for num in arr:
-            for num2 in arr:
-                if num + num2 == target:
-                    return [arr[num], arr[num2]]
-
-
-    # O(N)
     def twoSum2(nums, target):
         map = {}
         for i, n in enumerate(nums):

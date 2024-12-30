fc5e911c664e0742e60b3374fe4e257695c1e553
Shams Hasan <shamshasan941@gmail.com>
2024-12-30 17:58:20 -0500

    solved new leetcode problem using hashmaps; this is method 1 of solving

---
+++ coding-challenges/leetcode/easy/firstUniqueCharacter1.py
@@ -0,0 +1,21 @@
+## method 1 of solving first unique char in a string
+## used for loops & if/else statements
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        unique = -1 # store index of unique character
+        first_unique = {}
+
+        for i, c in enumerate(s): # loop through string, keep track of index & char
+            if c in first_unique: # if character is found in map
+                first_unique[c] = -1 # then it has been repeated
+            else:
+                first_unique[c] = i # otherwise, set its index
+
+        valid = [] # stores values greater than 1
+        for value in first_unique.values(): # loop through values of map
+            if value > -1: # if value hasnt been repeated
+                valid.append(value) # add it to our valid list
+                unique = min(valid) # find the minimum index
+        return unique # return minimum index
+            
